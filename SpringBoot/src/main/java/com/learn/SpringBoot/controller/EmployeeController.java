2418e0b607d4089c98ba7eacf7970e04d57258cc
Ajaypatel <Ajaypatelap1504@gmail.com>
2024-03-05 11:12:36 +0530

    CRUD Operations

--- SpringBoot/src/main/java/com/learn/SpringBoot/controller/EmployeeController.java
+++ SpringBoot/src/main/java/com/learn/SpringBoot/controller/EmployeeController.java
@@ -4,9 +4,7 @@ import com.learn.SpringBoot.Entity.Employee;
 import com.learn.SpringBoot.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,4 +19,26 @@ public class EmployeeController {
     public List<Employee> findAllEmployees(){
         return employeeService.getAllEmployee();
     }
+
+    @RequestMapping("/employees/{id}")
+    public Employee findAnEmployee(@PathVariable int id){
+        return employeeService.getEmployeeById(id);
+    }
+
+    @RequestMapping(value = "/employees",method = RequestMethod.POST)
+    public void createEmployee(@RequestBody Employee employee){
+        employeeService.createEmployee(employee);
+    }
+
+    @RequestMapping(value = "/employees/{id}",method = RequestMethod.PUT)
+    public void updateEmployee(@PathVariable int id,@RequestBody Employee employee){
+        employeeService.updateEmployee(employee);
+    }
+
+    @RequestMapping(value = "/employees/{id}",method = RequestMethod.DELETE)
+    public List<Employee> deleteEmployee(@PathVariable int id){
+        employeeService.deleteEmployee(id);
+        return employeeService.getAllEmployee();
+    }
+
 }
--- SpringBoot/src/main/java/com/learn/SpringBoot/service/EmployeeService.java
+++ SpringBoot/src/main/java/com/learn/SpringBoot/service/EmployeeService.java
@@ -4,17 +4,50 @@ package com.learn.SpringBoot.service;
 import com.learn.SpringBoot.Entity.Employee;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class EmployeeService {
 
-    List<Employee> employeeList = Arrays.asList(
+    List<Employee> employeeList = new ArrayList<>(Arrays.asList(
             new Employee(1, "First Employee", "Washington"),
             new Employee(2, "Second Employee", "New York")
-    );
+    ));
+
     public List<Employee> getAllEmployee() {
         return employeeList;
+    }
+
+    public Employee getEmployeeById(int id){
+        return employeeList.stream().filter(e -> e.getId() == id).findFirst().get();
+    }
+
+    public void createEmployee(Employee employee){
+        employeeList.add(employee);
+    }
+
+    public void updateEmployee(Employee employee){
+        List<Employee> temp = new ArrayList<>();
+        for (Employee emp : employeeList){
+            if (emp.getId() == employee.getId()){
+                emp.setEmpName(employee.getEmpName());
+                emp.setCity(employee.getCity());
+            }
+            temp.add(emp);
+        }
+        this.employeeList = temp;
+    }
+
+    public void deleteEmployee(int id){
+        List<Employee> temp = new ArrayList<>();
+        for (Employee emp : employeeList){
+            if (emp.getId() == id){
+                continue;
+            }
+            temp.add(emp);
+        }
+        this.employeeList = temp;
     }
 }
